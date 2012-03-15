b058f5da1112e39cc8360f5383393690d711a61c
Dalton Barreto <daltonmatos@gmail.com>
2012-03-15 20:08:23 -0300

    Funcionamento basico da montagem de queries SQL
    Já monta um select simples, com campos.
    
    select campo1, campo2 from tabela

---
+++ src/com/sixcodes/query/Query.java
@@ -0,0 +1,36 @@
+package com.sixcodes.query;
+
+public class Query {
+
+	private StringBuffer query;
+	
+	public Query() {
+		this.query = new StringBuffer();
+	}
+	
+	public Query select(String ... campos) {
+		this.query.append("SELECT ");
+		
+		if (campos.length > 0){
+			for (int i = 0; i < campos.length - 1; i++){
+				String c = campos[i];
+				this.query.append(c);
+				this.query.append(", ");
+			}
+			this.query.append(campos[campos.length - 1]);
+		}
+		return this;
+	}
+
+	public String toSql() {
+		return this.query.toString();
+	}
+
+	public Query from(String tabela) {
+		this.query.append(" FROM ");
+		this.query.append(tabela);
+		this.query.append(" ");
+		return this;
+	}
+
+}
---
+++ src/com/sixcodes/query/QueryTest.java
@@ -0,0 +1,49 @@
+package com.sixcodes.query;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueryTest {
+	
+	
+	private Query q;
+
+
+	@Before
+	public void setUp(){
+		q = new Query();
+	}
+
+	@Test
+	public void selectComCampos() {
+		Query select = q.select("a", "b", "c");
+		assertNotNull(select);
+		assertTrue(select == q);
+		assertEquals("SELECT a, b, c", select.toSql());
+	}
+
+	@Test
+	public void selectSemCampos() {
+		Query select = q.select();
+		assertNotNull(select);
+		assertTrue(select == q);
+		assertEquals("SELECT ", select.toSql());
+	}
+	
+	
+	@Test
+	public void from(){
+		q.from("tabela");
+		assertEquals(" FROM tabela ", q.toSql());
+	}
+	
+	@Test
+	public void selectFrom(){
+		q.select("id", "nome", "tamanho").from("arquivo");
+		assertEquals("SELECT id, nome, tamanho FROM arquivo ", q.toSql());
+	}
+	
+
+}
